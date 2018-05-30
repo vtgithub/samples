d738858a141e8293482ee419582e669205acadbe
vahid <dev.vahid@gmail.com>
2018-05-30 13:26:23 +0430

    react sample has been added

---
+++ react/tic-tac-to/src/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+
+    constructor(props){
+        super(props);
+        // this.state = {value:null};
+
+    }
+    render() {
+        return (
+            React.createElement(
+                // 'button', {className:'square', onClick:()=>this.setState({value:'X'})}, this.state.value
+                'button', { className:'square', onClick:()=>this.props.onClick() }, this.props.value
+            )
+        //     <button className="square">
+        //         {/* TODO */}
+        //     </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    counter = 0;
+    constructor(props){
+        super(props);
+        this.state = {squares:Array(9).fill(null)}
+    }
+
+    render() {
+        const status = "Next player: X";
+        return (
+            React.createElement('div',
+                React.createElement('div', {className:'status'}, status),
+                React.createElement('div', {className:'board-row'}, this.renderSquare(0), this.renderSquare(1), this.renderSquare(2)),
+                React.createElement('div', {className:'board-row'}, this.renderSquare(3), this.renderSquare(4), this.renderSquare(5)),
+                React.createElement('div', {className:'board-row'}, this.renderSquare(6), this.renderSquare(7), this.renderSquare(8))
+            )
+        );
+    }
+
+    renderSquare(i) {
+        return <Square
+            value={this.state.squares[i]}
+            onClick={()=>this.handleClick(i)}
+        />;
+    }
+
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = (this.counter++%2==0)?'O':'X';
+        this.setState({squares: squares});
+    }
+
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            React.createElement('div', {className:'game'},
+                React.createElement('div', {className:'game-board'}, <Board />),
+                React.createElement('div', {className:'game-board'},
+                    React.createElement('div'),
+                    React.createElement('ol')
+                )
+            )
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
